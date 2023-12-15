648a7679766d4f0b632883b4616dd6dff055bfea
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-12-15 12:43:09 +0500

    gfd

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,8 @@
 import productModel from "../models/productModel.js";
 
+import cloudinary from "cloudinary";
+import { getDataUri } from "./../utils/features.js";
+
 // GET ALL PRODUCTS
 export const getAllProductsController = async (req, res) => {
   try {
@@ -48,6 +51,145 @@ export const getSingleProductController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error In Get single Products API",
+      error,
+    });
+  }
+};
+
+// CREATE PRODUCT
+export const createProductController = async (req, res) => {
+  try {
+    const { name, description, price, category, stock } = req.body;
+    // // validtion
+    // if (!name || !description || !price || !stock) {
+    //   return res.status(500).send({
+    //     success: false,
+    //     message: "Please Provide all fields",
+    //   });
+    // }
+    if (!req.file) {
+      return res.status(500).send({
+        success: false,
+        message: "please provide product images",
+      });
+    }
+    const file = getDataUri(req.file);
+    const cdb = await cloudinary.v2.uploader.upload(file.content);
+    const image = {
+      public_id: cdb.public_id,
+      url: cdb.secure_url,
+    };
+
+    await productModel.create({
+      name,
+      description,
+      price,
+      category,
+      stock,
+      images: [image],
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "product Created Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Get single Products API",
+      error,
+    });
+  }
+};
+
+// UPDATE PRODUCT
+export const updateProductController = async (req, res) => {
+  try {
+    // find product
+    const product = await productModel.findById(req.params.id);
+    //valdiatiuon
+    if (!product) {
+      return res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    const { name, description, price, stock, category } = req.body;
+    // validate and update
+    if (name) product.name = name;
+    if (description) product.description = description;
+    if (price) product.price = price;
+    if (stock) product.stock = stock;
+    if (category) product.category = category;
+
+    await product.save();
+    res.status(200).send({
+      success: true,
+      message: "product details updated",
+    });
+  } catch (error) {
+    console.log(error);
+    // cast error ||  OBJECT ID
+    if (error.name === "CastError") {
+      return res.status(500).send({
+        success: false,
+        message: "Invalid Id",
+      });
+    }
+    res.status(500).send({
+      success: false,
+      message: "Error In Get UPDATE Products API",
+      error,
+    });
+  }
+};
+
+// UPDATE PRODUCT IMAGE
+export const updateProductImageController = async (req, res) => {
+  try {
+    // find product
+    const product = await productModel.findById(req.params.id);
+    // valdiation
+    if (!product) {
+      return res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    // check file
+    if (!req.file) {
+      return res.status(404).send({
+        success: false,
+        message: "Product image not found",
+      });
+    }
+
+    const file = getDataUri(req.file);
+    const cdb = await cloudinary.v2.uploader.upload(file.content);
+    const image = {
+      public_id: cdb.public_id,
+      url: cdb.secure_url,
+    };
+    // save
+    product.images.push(image);
+    await product.save();
+    res.status(200).send({
+      success: true,
+      message: "product image updated",
+    });
+  } catch (error) {
+    console.log(error);
+    // cast error ||  OBJECT ID
+    if (error.name === "CastError") {
+      return res.status(500).send({
+        success: false,
+        message: "Invalid Id",
+      });
+    }
+    res.status(500).send({
+      success: false,
+      message: "Error In Get UPDATE Products API",
       error,
     });
   }
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { isAuth } from "./../middlewares/authMiddleware.js";
 import {
+  createProductController,
   getAllProductsController,
   getSingleProductController,
+  updateProductController,
+  updateProductImageController,
 } from "../controllers/productController.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -12,5 +17,14 @@ router.get("/get-all", getAllProductsController);
 
 // GET SINGLE PRODUCTS
 router.get("/:id", getSingleProductController);
+
+// CREATE PRODUCT
+router.post("/create", isAuth, singleUpload, createProductController);
+
+// UPDATE PRODUCT
+router.put("/:id", isAuth, updateProductController);
+
+// UPDATE PRODUCT IMAGE
+router.put("/image/:id", isAuth, singleUpload, updateProductImageController);
 
 export default router;
